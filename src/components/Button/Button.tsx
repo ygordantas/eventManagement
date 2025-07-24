217a947e85c1261e6a7ccb841b42686af9836efd
Aleksandr Feshchuk <aleksfeschuk@gmail.com>
2025-07-23 21:34:47 -0600

    handle disabled state for any HTML element via s prop

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,32 +1,49 @@
-import type { PropsWithChildren } from "react";
+import { useRef, type PropsWithChildren } from "react";
 import classes from "./Button.module.css";
 import createClassName from "../../utils/createClassName";
 
 type ButtonProps<T extends React.ElementType> = {
   as?: T;
   variant?: "solid" | "outline";
+  disabled?: boolean;
 } & React.ComponentPropsWithRef<T>;
 
+
 export default function Button<T extends React.ElementType = "button">({
-  ref,
   children,
   as = "button",
   variant = "solid",
+  disabled = false,
+  className,
   ...props
 }: PropsWithChildren<ButtonProps<T>>) {
-  const Component = as as React.ElementType;
-
-  const { className, ...remainingProps } = props;
-
+  
+  const Component = as;
+  const internalRef = useRef<HTMLElement>(null);
+  
   const btnClasses = createClassName(
     className,
     classes.btn,
-    variant === "solid" ? classes.solid : classes.outline
+    variant === "solid" ? classes.solid : classes.outline,
+    disabled && classes.disabled
   );
 
   return (
-    <Component ref={ref} className={btnClasses} {...remainingProps}>
-      {children}
+    <Component
+      {...props} 
+      ref={internalRef} 
+      className={btnClasses} 
+      disabled = {as === "button" ? disabled : undefined}
+      aria-disabled={disabled}
+      tabIndex={disabled ? -1 : undefined}
+      onClick={(e: React.MouseEvent<HTMLElement>) => {
+        if (disabled) {
+          e.preventDefault();
+          e.stopPropagation();
+        }
+        props.onClick?.(e);
+      }}>
+        {children}
     </Component>
   );
 }
