7c2bca1aa7f2d295e08c482ccdb1089e5d9b3d06
ygordantas <ygord.furtado@gmail.com>
2025-07-26 15:40:48 -0600

    added home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { Route, Routes } from "react-router";
 import AuthLayout from "./components/AuthLayout/AuthLayout";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegistrationPage from "./pages/RegistrationPage/RegistrationPage";
+import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   return (
     <Routes>
+      <Route index element={<HomePage />} />
       <Route element={<AuthLayout />}>
         <Route path="login" element={<LoginPage />} />
         <Route path="register" element={<RegistrationPage />} />
