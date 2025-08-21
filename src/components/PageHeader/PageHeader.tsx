68a0289d8230a70125c78eadd8b6c3cda3fef77e
Furtado, Ygor <yfurtado@obsglobal.com>
2025-08-21 17:32:02 -0600

    created Page header component

---
+++ src/components/PageHeader/PageHeader.tsx
@@ -0,0 +1,15 @@
+import type { PropsWithChildren } from "react";
+import classes from "./PageHeader.module.css";
+
+interface PageHeaderProps {
+  title: string;
+}
+
+export default function PageHeader({ title, children }: PropsWithChildren<PageHeaderProps>) {
+  return (
+    <div className={classes.header}>
+      <h1 className={classes.title}>{title}</h1>
+      {children && <div className={classes.actions}>{children}</div>}
+    </div>
+  );
+}
