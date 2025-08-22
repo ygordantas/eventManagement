e37d86bc85d31d036d4d40bd4df1f85bb194ec64
ygordantas <ygord.furtado@gmail.com>
2025-08-21 20:25:02 -0600

    created event page

---
+++ src/constants/dateFiltersTypes.ts
@@ -0,0 +1,10 @@
+const DATE_FILTER_TYPES = {
+  past: "Past",
+  now: "Now",
+  upcoming: "Upcoming",
+  all: "All",
+};
+
+export type DateFilterType = "Past" | "Now" | "Upcoming" | "All";
+
+export default DATE_FILTER_TYPES;
--- src/pages/EventsPage/EventsPage.tsx
+++ src/pages/EventsPage/EventsPage.tsx
@@ -2,23 +2,27 @@ import { useEffect, useState } from "react";
 import type EventModel from "../../models/EventModel";
 import eventsServices from "../../services/eventsServices";
 import useAlertContext from "../../hooks/useAlertContext";
+import PageHeader from "../../components/PageHeader/PageHeader";
+import EventGrid from "../../components/EventGrid/EventGrid";
+import EventCard from "../../components/EventCard/EventCard";
+import Button from "../../components/Button/Button";
+import Select from "../../components/Select/Select";
+import type { DateFilterType } from "../../constants/dateFiltersTypes";
+import DATE_FILTER_TYPES from "../../constants/dateFiltersTypes";
+import { getDateOnlyString } from "../../utils/dateUtils";
 
 export default function EventsPage() {
   const { showErrorAlert } = useAlertContext();
-  const [upcomingEvents, setUpcomingEvents] = useState<EventModel[]>([]);
+
+  const [allEvents, setAllEvents] = useState<EventModel[]>([]);
+  const [filter, setFilter] = useState<DateFilterType | undefined>();
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const getUpcomingEvents = async () => {
+    const getAllEvents = async () => {
       try {
         const events = await eventsServices.getAllEvents();
-        setUpcomingEvents(
-          events.sort(
-            (a, b) =>
-              new Date(a.date).getMilliseconds() -
-              new Date(b.date).getMilliseconds()
-          )
-        );
+        setAllEvents(events);
       } catch (error) {
         showErrorAlert(error);
       } finally {
@@ -26,25 +30,65 @@ export default function EventsPage() {
       }
     };
 
-    getUpcomingEvents();
+    getAllEvents();
   }, [showErrorAlert]);
 
   return isLoading ? (
     "Loading..."
   ) : (
-    <div>
-      EventsPage
-      {upcomingEvents.length > 0 ? (
-        <ul>
-          {upcomingEvents.map((e) => (
-            <li key={e.id}>
-              {e.name} <strong>{e.date}</strong>
-            </li>
+    <>
+      <PageHeader title="Events">
+        <Select
+          label={"Filter by date"}
+          onChange={(e) =>
+            setFilter(
+              e.target.value ? (e.target.value as DateFilterType) : undefined
+            )
+          }
+          options={Object.values(DATE_FILTER_TYPES).map((v) => ({
+            code: v,
+            value: v,
+          }))}
+        />
+      </PageHeader>
+      <EventGrid>
+        {allEvents
+          .filter((e) => {
+            if (filter === DATE_FILTER_TYPES.all) return true;
+
+            const todayUTC = new Date(new Date().getTime());
+            todayUTC.setHours(0);
+            todayUTC.setMinutes(0);
+            todayUTC.setSeconds(0);
+            todayUTC.setMilliseconds(0);
+
+            const [year, month, day] = e.date.split("-").map(Number);
+            const eventDate = new Date(year, month - 1, day);
+
+            switch (filter) {
+              case DATE_FILTER_TYPES.now:
+                return (
+                  getDateOnlyString(eventDate) === getDateOnlyString(todayUTC)
+                );
+              case DATE_FILTER_TYPES.past:
+                return eventDate < todayUTC;
+              case DATE_FILTER_TYPES.upcoming:
+                return eventDate > todayUTC;
+              default:
+                return eventDate >= todayUTC;
+            }
+          })
+          .sort(
+            (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()
+          )
+          .map((e) => (
+            <EventCard
+              key={e.id}
+              event={e}
+              footer={<Button>Attend Event</Button>}
+            />
           ))}
-        </ul>
-      ) : (
-        "No upcoming events... "
-      )}
-    </div>
+      </EventGrid>
+    </>
   );
 }
