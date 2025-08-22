af668668864fb48c51be503ed4b54417adcae379
ygordantas <ygord.furtado@gmail.com>
2025-08-21 20:24:53 -0600

    updated my events page

--- src/pages/MyEventsPage/MyEventsPage.tsx
+++ src/pages/MyEventsPage/MyEventsPage.tsx
@@ -1,11 +1,13 @@
 import eventsServices from "../../services/eventsServices";
 import useAuthContext from "../../hooks/useAuthContext";
-import classes from "./MyEventsPage.module.css";
 import Button from "../../components/Button/Button";
 import { Link } from "react-router";
 import { useEffect, useState } from "react";
 import type EventModel from "../../models/EventModel";
 import useAlertContext from "../../hooks/useAlertContext";
+import EventGrid from "../../components/EventGrid/EventGrid";
+import EventCard from "../../components/EventCard/EventCard";
+import PageHeader from "../../components/PageHeader/PageHeader";
 
 export default function MyEventsPage() {
   const { user } = useAuthContext();
@@ -50,74 +52,30 @@ export default function MyEventsPage() {
   return isLoading ? (
     "Loading..."
   ) : (
-    <div className={classes.container}>
-      <div className={classes.header}>
-        <h1 className={classes.title}>My Events</h1>
+    <>
+      <PageHeader title="My Events">
         <Button as={Link} to="/my-events/manage">
           Create new event
         </Button>
-      </div>
-
-      <div className={classes.eventsGrid}>
+      </PageHeader>
+      <EventGrid>
         {myEvents.map((e) => (
-          <div key={e.id} className={classes.eventCard}>
-            <div className={classes.eventHeader}>
-              <h3 className={classes.eventTitle}>{e.name}</h3>
-              <span className={classes.status}>status</span>
-            </div>
-
-            <div className={classes.eventDetails}>
-              <p className={classes.eventDateTime}>
-                {e.date} - {e.time} - {e.timezoneCode}
-              </p>
-              {e.isOnline ? (
-                <a href={e.address} target="_blank">
-                  {e.address}
-                </a>
-              ) : (
-                <p className={classes.eventLocation}>{e.address}</p>
-              )}
-              {e.description && (
-                <p className={classes.description}>
-                  {e.description.length > 100
-                    ? e.description.substring(0, 100) + "..."
-                    : e.description}
-                </p>
-              )}
-            </div>
-
-            <div className={classes.eventMeta}>
-              <div className={classes.metaItem}>
-                <span className={classes.metaLabel}>Visibility:</span>
-                <span className={classes.metaValue}>
-                  {e.isPrivate ? "Private" : "Public"}
-                </span>
-              </div>
-
-              <div className={classes.metaItem}>
-                <span className={classes.metaLabel}>Price:</span>
-                <span className={classes.metaValue}>
-                  {e.entrancePrice ? e.entrancePrice : "Free"}
-                </span>
-              </div>
-
-              {e.maxCapacity && (
-                <div className={classes.metaItem}>
-                  <span className={classes.metaLabel}>Max Capacity:</span>
-                  <span className={classes.metaValue}>{e.maxCapacity}</span>
-                </div>
-              )}
-            </div>
-
-            <div className={classes.eventActions}>
-              <Button as={Link} to={`/my-events/manage/${e.id}`}>
-                Edit
-              </Button>
-              <Button onClick={() => onDeleteEventHandler(e.id)}>Remove</Button>
-            </div>
-          </div>
+          <EventCard
+            key={e.id}
+            event={e}
+            footer={
+              <>
+                <Button as={Link} to={`/my-events/manage/${e.id}`}>
+                  Edit
+                </Button>
+                <Button onClick={() => onDeleteEventHandler(e.id)}>
+                  Remove
+                </Button>
+              </>
+            }
+          />
         ))}
-      </div>
-    </div>
+      </EventGrid>
+    </>
   );
 }
